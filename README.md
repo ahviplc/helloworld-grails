b9d3b70a34a810b26ac8c1e02f85e8ed95bb3bd4
ahviplc <ahlc@sina.cn>
2023-04-13 11:11:30 +0800

    chore: 第一个api请求

---
+++ README.md
@@ -0,0 +1,3 @@
+# helloworld-grails
+
+> TODO
---
+++ grails-app/controllers/hello/HelloController.groovy
@@ -0,0 +1,8 @@
+// 文件路径：grails-app/controllers/hello/HelloController.groovy
+package hello
+
+class HelloController {
+    def index() {
+        render "Hello, World!hello"
+    }
+}
